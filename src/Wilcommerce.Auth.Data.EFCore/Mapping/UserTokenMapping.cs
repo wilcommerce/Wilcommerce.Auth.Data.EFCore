60630d40f332ac67b2fef5ec0b9e1cbd41d19a8b
albi.mori <albi.mori@gmail.com>
2017-12-22 12:33:47 +0100

    add context and mapping

---
+++ src/Wilcommerce.Auth.Data.EFCore/Mapping/UserTokenMapping.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wilcommerce.Auth.Models;
+
+namespace Wilcommerce.Auth.Data.EFCore.Mapping
+{
+    public static class UserTokenMapping
+    {
+        public static ModelBuilder MapUserToken(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserToken>()
+                .ToTable("Wilcommerce_UserTokens");
+
+            return modelBuilder;
+        }
+    }
+}
